bf2ebfce4828736314994e9e697f38ee3b9baf08
Bhavitha-reddy <93107650+Bhavitha-reddy@users.noreply.github.com>
2021-12-03 14:45:40 +0530

    Update p2original.c

---
+++ p2original.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter the value:\n");
+  scanf("%d",&a);
+  return a;
+}
+int add(int a,int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+int output(int a,int b,int sum)
+{
+  printf("sum of a and b is c,%d,%d,%d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum);
+  return 0;
+}
